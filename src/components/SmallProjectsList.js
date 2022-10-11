469472e7c904610e1fa54397ef10da0b628099b0
Owen <owenpickard89@gmail.com>
2022-10-11 10:53:54 -0400

    Reorder small projects, rm image watermarker

--- src/components/SmallProjectsList.js
+++ src/components/SmallProjectsList.js
@@ -1,6 +1,28 @@
 
 const smallerProjectsList = [
     {
+        id: 5,
+        title: "NFL Data Analysis",
+        imgURL: "https://images.pexels.com/photos/128457/pexels-photo-128457.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Program scrapes data from ESPN and visualizes player statistics using graphs.",
+        tagOne: "Data visualization with Plotly and Matplotlib",
+        tagTwo: "Web scraping with Beautiful Soup",
+        tagThree: "Data organization with pandas",
+        gitURL: "https://github.com/citysushi89/small_projects/tree/main/NFLDataAnalysis",
+
+    }, 
+    {
+        id: 1,
+        title: "Encoder/Decoder",
+        imgURL: "https://images.pexels.com/photos/7319078/pexels-photo-7319078.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Uses your choice of Morse Code or the Caesar Cipher to encode and decode text.",
+        tagOne: "Basic Cryptography",
+        tagTwo: "Relies on internal dictionaries/lists for decoding",
+        tagThree: "Copies encoded or decoded message to clipboard once completed",
+        gitURL: "https://github.com/citysushi89/small_projects/blob/main/encoderDecoder.py",
+
+    },
+    {
         id: 8,
         title: "Selection of Small Book Projects",
         imgURL: "https://images.pexels.com/photos/159866/books-book-pages-read-literature-159866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
@@ -9,6 +31,15 @@ const smallerProjectsList = [
         tagTwo: "Clickbait Headline Generator",
         tagThree: "Countdown",
         gitURL: "https://github.com/citysushi89/small_projects/tree/main/Book%20Projects",
+
+    },
+    {
+        id: 6,
+        title: "Unit Testing",
+        imgURL: "https://images.pexels.com/photos/6684255/pexels-photo-6684255.jpeg?cs=srgb&dl=pexels-andy-barbour-6684255.jpg&fm=jpg",
+        description: "Program contains basic math operations and uses the unittest module to assess their effectiveness.",
+        tagOne: "Unit Tests",
+        gitURL: "https://github.com/citysushi89/small_projects/tree/main/unittest",
 
     },
     {
@@ -22,17 +53,7 @@ const smallerProjectsList = [
         gitURL: "https://github.com/citysushi89/small_projects/tree/main/SpaceInvaders",
 
     },
-    {
-        id: 1,
-        title: "Encoder/Decoder",
-        imgURL: "https://images.pexels.com/photos/7319078/pexels-photo-7319078.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        description: "Uses your choice of Morse Code or the Caesar Cipher to encode and decode text.",
-        tagOne: "Basic Cryptography",
-        tagTwo: "Relies on internal dictionaries/lists for decoding",
-        tagThree: "Copies encoded or decoded message to clipboard once completed",
-        gitURL: "https://github.com/citysushi89/small_projects/blob/main/encoderDecoder.py",
 
-    },
     // {
     //     id: 2,
     //     title: "BlackJack",
@@ -44,17 +65,17 @@ const smallerProjectsList = [
     //     gitURL: "https://github.com/citysushi89/small_projects/blob/main/blackjack.py",
 
     // },
-    {
-        id: 3,
-        title: "Image Watermarker",
-        imgURL: "https://images.pexels.com/photos/220213/pexels-photo-220213.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        description: "Takes a picture and a watermark and returns a watermarked image.",
-        tagOne: "Tag concepts / libraries / languages used",
-        tagTwo: "ie Python",
-        tagThree: "ie pyperclip",
-        gitURL: "",
+    // {
+    //     id: 3,
+    //     title: "Image Watermarker",
+    //     imgURL: "https://images.pexels.com/photos/220213/pexels-photo-220213.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    //     description: "Takes a picture and a watermark and returns a watermarked image.",
+    //     tagOne: "Tag concepts / libraries / languages used",
+    //     tagTwo: "ie Python",
+    //     tagThree: "ie pyperclip",
+    //     gitURL: "",
 
-    },
+    // },
     {
         id: 4,
         title: "Dino Game Automation",
@@ -66,26 +87,6 @@ const smallerProjectsList = [
         gitURL: "https://github.com/citysushi89/small_projects/blob/main/dino.py",
 
     },
-    {
-        id: 5,
-        title: "NFL Data Analysis",
-        imgURL: "https://images.pexels.com/photos/128457/pexels-photo-128457.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        description: "Program scrapes data from ESPN and visualizes player statistics using graphs.",
-        tagOne: "Data visualization with Plotly and Matplotlib",
-        tagTwo: "Web scraping with Beautiful Soup",
-        tagThree: "Data organization with pandas",
-        gitURL: "https://github.com/citysushi89/small_projects/tree/main/NFLDataAnalysis",
-
-    }, 
-    {
-        id: 6,
-        title: "Unit Testing",
-        imgURL: "https://images.pexels.com/photos/6684255/pexels-photo-6684255.jpeg?cs=srgb&dl=pexels-andy-barbour-6684255.jpg&fm=jpg",
-        description: "Program contains basic math operations and uses the unittest module to assess their effectiveness.",
-        tagOne: "Unit Tests",
-        gitURL: "https://github.com/citysushi89/small_projects/tree/main/unittest",
-
-    }
 
 ];
 
