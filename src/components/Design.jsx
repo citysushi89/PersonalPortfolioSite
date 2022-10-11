0ede8e15ea76a313193621bdb8564c1371770618
Owen <owenpickard89@gmail.com>
2022-10-11 09:42:46 -0400

    Change wording on Design.jsx

--- src/components/Design.jsx
+++ src/components/Design.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Design() {
     return (
     <div className="nocolor home-page-section gray-section">
-        <h1 className="page-title">Fancy Design goes here mmmmm</h1>
+        <h1 className="page-title">Welcome!</h1>
 
 
      </div>
