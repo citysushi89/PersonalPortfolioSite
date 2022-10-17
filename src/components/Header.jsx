03a838f8e3c8c4ca717c37bbc8953f0f15c1c61f
Owen <owenpickard89@gmail.com>
2022-10-17 17:58:44 -0400

    add favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,8 @@ function Header() {
 
         {/* My styles.css  */}
         <link rel="stylesheet" href="styles.css" />
-
+        {/* Favicon */}
+        <link rel="favicon" href='public/favicon.ico?v=2'/>
 
         <Navbar collapseOnSelect expand="lg" className="dark-section header">
             <Container>
