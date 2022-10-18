64e1e8d53e7aabf38651a7e9bc76783463737bd5
Owen <owenpickard89@gmail.com>
2022-10-18 14:15:36 -0400

    favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,8 +24,8 @@ function Header() {
         {/* My styles.css  */}
         <link rel="stylesheet" href="styles.css" />
         {/* Favicon */}
-        <link rel="favicon" href='public/favicon.ico?v=2'/>
-
+        {/* <link rel="favicon" href='public/favicon.ico?v=2'/> */}
+        <link rel="shortcut icon" type="image/jpg" href="public/favicon.ico?v=2"/>
 
         <Navbar collapseOnSelect expand="lg" className="dark-section header">
             <Container>
