57dc0557486da13f0a334047b9b7cacf35db7308
Owen <owenpickard89@gmail.com>
2022-10-18 11:18:34 -0400

    icon links broken, testing new relative paths

--- src/components/Design.jsx
+++ src/components/Design.jsx
@@ -4,13 +4,13 @@ function Design() {
     return (
     <div className="nocolor home-page-section gray-section extra-margin">
         <h1 className="page-title">Welcome!</h1>
-        <img className='welcome-img python-icon' src='public\images\python.png'/>
-        <img className='welcome-img comp-icon' src='public\images\programming.png'/>
-        <img className='welcome-img data-icon' src='public\images\analysis.png'/>
-        <img className='welcome-img html-icon' src='public\images\html.png'/>
+        <img className='welcome-img python-icon' src='../../public/images/python.png'/>
+        <img className='welcome-img comp-icon' src='..\public\images\programming.png'/>
+        <img className='welcome-img data-icon' src='../public/images/analysis.png'/>
+        <img className='welcome-img html-icon' src='..\..\public\images\html.png'/>
         <img className='welcome-img web-auto-icon' src='public\images\automation.png'/>
         <img className='welcome-img gear-icon' src='public\images\engineering.png'/>
-        <a href="https://cdn-icons-png.flaticon.com/128/5968/5968350.png" title="python icons"></a>
+        {/* <a href="https://cdn-icons-png.flaticon.com/128/5968/5968350.png" title="python icons"></a> */}
 
      </div>
     );
