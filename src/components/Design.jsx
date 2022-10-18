b0a91ceea993b559e03ffe5f3654f7e96d0e98a1
Owen <owenpickard89@gmail.com>
2022-10-18 12:10:39 -0400

    change from relative paths to links for icons

--- src/components/Design.jsx
+++ src/components/Design.jsx
@@ -5,16 +5,13 @@ function Design() {
     return (
     <div className="nocolor home-page-section gray-section extra-margin">
         <h1 className="page-title">Welcome</h1>
-        <img src='../../public/images/python.png' className='welcome-img python-icon' />
-        <img src='..\public\images\programming.png' className='welcome-img comp-icon' />
+        <img src='https://cdn-icons-png.flaticon.com/512/5968/5968350.png' className='welcome-img python-icon' alt="python-icon" />
+        <img src='https://cdn-icons-png.flaticon.com/512/6062/6062646.png' className='welcome-img comp-icon' alt="python-icon"/>
         {/* https://cdn-icons-png.flaticon.com/512/5968/5968350.png */}
-        <img src='../public/images/analysis.png' className='welcome-img data-icon' />
-        <img src='..\..\public\images\html.png' className='welcome-img html-icon' />
-        <img src='public\images\automation.png' className='welcome-img web-auto-icon' />
-        <img src='public/images/engineering.png' className='welcome-img gear-icon' ></img>
-        <img src='https://cdn-icons-png.flaticon.com/512/5968/5968350.png' className="welcome-img" alt="..."  />
-        {/* <a href="https://cdn-icons-png.flaticon.com/128/5968/5968350.png" title="python icons"></a> */}
-
+        <img src='https://cdn-icons-png.flaticon.com/512/2318/2318736.png' className='welcome-img data-icon' alt="data-icon"/>
+        <img src='https://cdn-icons-png.flaticon.com/512/1183/1183671.png' className='welcome-img html-icon' alt="html-icon" />
+        <img src='https://cdn-icons-png.flaticon.com/512/2103/2103800.png' className='welcome-img web-auto-icon' alt="web-auto-ico"/>
+        <img src='https://cdn-icons-png.flaticon.com/512/3079/3079162.png' className='welcome-img gear-icon' alt="gear-icon"></img>
      </div>
     );
 }
