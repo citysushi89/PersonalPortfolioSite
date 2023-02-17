eb51fccc199f0d8ed6e031d4d3faa33a2d95e7b9
citysushi89 <owenpickard89@gmail.com>
2023-02-17 14:16:00 -0500

    add done-ish projects to mobiledevlist.js

--- src/pages/MobileDev.jsx
+++ src/pages/MobileDev.jsx
@@ -7,26 +7,24 @@ function MobileDev(props) {
     return (
 
     <div className="small-projects-section">   
-        <div className="card-deck">
-            <div className="cardList">
-                <div className="card mobile-card">
-                    {/* <div className="card-body"> */}
-                        <h5 className="card-title">{props.title}</h5>
-                        <p className="card-text">{props.description}</p>
-                    {/* </div> */}
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item card-list-item">{props.tagOne}</li>
-                        <li className="list-group-item card-list-item">{props.tagTwo}</li>
-                        <li className="list-group-item card-list-item">{props.tagThree}</li>
-                    </ul>
-                    <div className="card-body">
-                        <a href={props.gitURL} className="card-link">Github</a>
-                        <a href={props.vidURL} className="card-link">Usage</a>
-                    </div>
+        <div className="cardList">
+            <div className="card mobile-card">
+                {/* <div className="card-body"> */}
+                    <h5 className="card-title">{props.title}</h5>
+                    <p className="card-text">{props.description}</p>
+                {/* </div> */}
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item card-list-item">{props.tagOne}</li>
+                    <li className="list-group-item card-list-item">{props.tagTwo}</li>
+                    <li className="list-group-item card-list-item">{props.tagThree}</li>
+                </ul>
+                <div className="card-body">
+                    <a href={props.gitURL} className="card-link">Github</a>
+                    <a href={props.vidURL} className="card-link">Usage</a>
                 </div>
             </div>
-        </div> 
-    </div>      
+        </div>
+    </div>     
     
     );
 }
